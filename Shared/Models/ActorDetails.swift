abe9e36cbbe1edb405129ecf39eb1d3deb49a0a0
Ahmed Nasser <ahmed.nasser2310@gmail.com>
2022-06-09 17:45:29 +0200

    Update ActorDetails.swift

--- Shared/Models/ActorDetails.swift
+++ Shared/Models/ActorDetails.swift
@@ -19,6 +19,7 @@ struct Cast: Codable,Identifiable {
     let knownForDepartment, name, originalName: String?
     let profilePath: String?
     
+    
     public var ProfilePathUrl: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(profilePath ?? "")") ?? URL(string: "https://www.pexels.com/photo/person-holding-photo-of-single-tree-at-daytime-1252983/")!
     }
